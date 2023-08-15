21e99839a41af4496fdcb7d8328ccf4b49abd133
alfian <gusjabungaf@gmail.com>
2023-08-15 20:48:17 +0700

    add logout features

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -52,9 +52,10 @@ class UserController extends Controller
      
     }
 
-    public function doLogout()
+    public function doLogout(Request $request): RedirectResponse
     {
-
+        $request->session()->forget('user');
+        return redirect('/');
     }
 
 }
